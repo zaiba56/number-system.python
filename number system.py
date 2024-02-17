bfca6b848b29039ab368099f0c395348898d17ff
zaiba56 <160221536+zaiba56@users.noreply.github.com>
2024-02-17 16:36:43 +0530

    3rd project

---
+++ number system.py
@@ -0,0 +1,17 @@
+a=input('enter the number:') 
+num=""
+n=""
+for x in a:
+  match x:
+   case '0':
+     num='zero'
+   case '1':
+     num='one'
+   case '2':
+     num='two'
+   case '3':
+     num='three'
+   case '4':
+     num='four'
+   case '5':
+    num='five'
